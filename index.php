6f46be890d490c7d88c590d0460ef1e849e2eb60
leodine <64403877+leodine@users.noreply.github.com>
2020-05-15 10:49:14 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Joyeux" => [
+        "city" => "Anglet",
+        "weapon" => "Machette"
+    ]
+];
+
+var_dump($characters);
+
+?>
